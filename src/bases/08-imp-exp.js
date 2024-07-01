8932d134106e50ad30107c3daad04a3bf4c528ba
Klee Collage <klee.collage@gmail.com>
2024-07-01 16:01:09 -0600

    Cambios listos para merge
    Se agrega el archivo promesas a la carpeta bases

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,6 +1,5 @@
 // import { heroes } from './data/heroes' // snippet = imp
 // import {heroes} from "./data/heroes" // escribiendo el nombre del const aparece automaticamente
-<<<<<<< HEAD
 // import { heroes, owners } from './data/heroes
 import heroes, { owners } from "../data/heroes"; // se debe poner el impor t default sin corchetes
 
@@ -15,19 +14,3 @@ export const getHeroeById = (id) => {
 export const getHeroesByOwner = (owner) =>
 	heroes.filter((heroes) => heroes.owner === owner);
 // console.log(getHeroesByOwner("Marvel"));
-=======
-// import { heroes, owners } from './data/heroes' // esto se logra con el export default en la data
-import heroes, { owners } from './data/heroes'
-
-console.log(owners)
-console.log( heroes )
-
-const getHeroeById = (id) => {
-    return heroes.find((heroe) => heroe.id === id)
-}
-console.log(getHeroeById(4))
-
-const getHeroesByOwner = (owner) => heroes.filter((heroes) => heroes.owner === owner)
-console.log(getHeroesByOwner('Marvel'))
-
->>>>>>> a8a4c76 (imports & exports)
