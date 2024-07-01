3587c668aba2dee2333060bd7a950cb01355a2a6
Klee Collage <klee.collage@gmail.com>
2024-07-01 16:01:09 -0600

    Cambios listos para merge
    Se agrega el archivo promesas a la carpeta bases

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,16 +1,16 @@
 // import { heroes } from './data/heroes' // snippet = imp
 // import {heroes} from "./data/heroes" // escribiendo el nombre del const aparece automaticamente
-// import { heroes, owners } from './data/heroes' // esto se logra con el export default en la data
-import heroes, { owners } from './data/heroes'
+// import { heroes, owners } from './data/heroes
+import heroes, { owners } from "../data/heroes"; // se debe poner el impor t default sin corchetes
 
-console.log(owners)
-console.log( heroes )
+// console.log(owners);
+// console.log(heroes);
 
-const getHeroeById = (id) => {
-    return heroes.find((heroe) => heroe.id === id)
-}
-console.log(getHeroeById(4))
+export const getHeroeById = (id) => {
+	return heroes.find((heroe) => heroe.id === id);
+};
+// console.log(getHeroeById(4));
 
-const getHeroesByOwner = (owner) => heroes.filter((heroes) => heroes.owner === owner)
-console.log(getHeroesByOwner('Marvel'))
-
+export const getHeroesByOwner = (owner) =>
+	heroes.filter((heroes) => heroes.owner === owner);
+// console.log(getHeroesByOwner("Marvel"));
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-Esta linea de la rama main debe ser borrada 
-cuando se ejecute el Rebase 
-borrar borrar
+# ESTA LINEA ESTA EN LA BRANCH PROMISES
