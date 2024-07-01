4ff7f02aaf8c25e1818887f8e6582e1ea1216455
Klee Collage <klee.collage@gmail.com>
2024-06-30 18:45:52 -0600

    Se suben errores en el index
    Se crean diferencias en la rama con el archivo index para probar el comando rebase

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-//import { heroes } from './data/heroes' // snippet = imp
-import { heroes } from "./data/heroes" // escribiendo el nombre del const aparece automaticamente
-
-console.log( heroes )
-
-const getHeroeById = (id) => {
-    return heroes.find((heroe) => heroe.id === id)
-}
-console.log(getHeroeById(4))
-
-const getHeroesByOwner = (owner) => heroes.filter((heroes) => heroes.owner === owner)
-console.log(getHeroesByOwner('Marvel'))
-
+Esta linea de la rama main debe ser borrada 
+cuando se ejecute el Rebase 
+borrar borrar
